f75b0fdf26a2c837aab0108713ee2eb0dd6c195f
Andrew Sheldrick <andrew.sheldrick@precisionimpacts.com>
2026-02-24 16:09:14 -0500

    Hardcode supabase credentials

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'; 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL; 
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY; 
-export const supabase = createClient(supabaseUrl, supabaseKey); 
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://apnavcjdugxjtuijhxes.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwbmF2Y2pkdWd4anR1aWpoeGVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NTczMTIsImV4cCI6MjA4NzUzMzMxMn0.mA5oabMryU0E2ImAm5mkrZoWmCCrFJQIKvPdyVLM8pA';
+export const supabase = createClient(supabaseUrl, supabaseKey);
